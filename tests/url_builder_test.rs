326c69c95d2abf90ad6dcdddc7fd6785cf5d3526
BppleMan <1286363484@qq.com>
2025-07-05 11:24:27 +0800

    <update> 优化 UrlBuilder 模块的测试用例，新增对规则集 URL 的构建和解析测试，改进订阅 URL 的构建逻辑，增强对服务 URL 的验证，确保政策解析的正确性

--- tests/url_builder_test.rs
+++ tests/url_builder_test.rs
@@ -1,5 +1,7 @@
 use convertor::client::Client;
 use convertor::encrypt::decrypt;
+use convertor::profile::core::policy::Policy;
+use convertor::server::router::ProfileQuery;
 use convertor::subscription::url_builder::UrlBuilder;
 use std::collections::HashMap;
 use tracing::warn;
@@ -17,17 +19,27 @@ pub fn test_url_builder() -> color_eyre::Result<()> {
     let url_builder = UrlBuilder::new(server.clone(), secret, service_url.clone())?;
 
     let convertor_url = url_builder.build_convertor_url(Client::Surge)?;
-    println!("{}", convertor_url);
     pretty_assertions::assert_str_eq!("http", convertor_url.scheme());
     pretty_assertions::assert_str_eq!("127.0.0.1", convertor_url.host_str().expect("必须有 host"));
     pretty_assertions::assert_str_eq!("8001", convertor_url.port().expect("必须有 port").to_string());
     let query_pairs = convertor_url.query_pairs().collect::<HashMap<_, _>>();
     pretty_assertions::assert_str_eq!(
-        "https://example.com/subscription?token=12345",
+        service_url.as_str(),
         decrypt(secret.as_bytes(), &query_pairs["raw_url"])?
     );
 
-    let sub_url = url_builder.build_subscription_url(Client::Surge)?;
-    pretty_assertions::assert_str_eq!(format!("{}&flag=surge", service_url.as_str()), sub_url.as_str());
+    let subscription_url = url_builder.build_subscription_url(Client::Surge)?;
+    pretty_assertions::assert_str_eq!(
+        format!("{}&flag=surge", service_url.as_str()),
+        subscription_url.as_str()
+    );
+
+    let rule_set_url = url_builder.build_rule_set_url(Client::Surge, &Policy::subscription_policy())?;
+    let query: ProfileQuery = serde_qs::from_str(rule_set_url.query().as_ref().unwrap())?;
+    pretty_assertions::assert_str_eq!("DIRECT", &query.policy.as_ref().unwrap().name);
+    pretty_assertions::assert_str_eq!(
+        "true",
+        query.policy.as_ref().unwrap().is_subscription.to_string().as_str()
+    );
     Ok(())
 }
