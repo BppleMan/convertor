14982480d3ec53cc927f42f81716fede131e2f5b
BppleMan <1286363484@qq.com>
2025-09-08 20:32:40 +0800

    <feat> 新增订阅表单的表单控件与验证逻辑，重构样式以优化用户体验

---
+++ dashboard/src/app/common/model/enums.ts
@@ -0,0 +1,8 @@
+export enum ProxyClient {
+    Surge = "Surge",
+    Clash = "Clash",
+}
+
+export enum SubProvider {
+    BosLife = "Boslife",
+}
--- dashboard/src/app/page/dashboard/dashboard-sub/dashboard-sub.ts
+++ dashboard/src/app/page/dashboard/dashboard-sub/dashboard-sub.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
-import { FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { MatButton } from "@angular/material/button";
 import { MatFormField, MatLabel } from "@angular/material/form-field";
 import { MatInput } from "@angular/material/input";
 import { MatOption, MatSelect } from "@angular/material/select";
 import { MatSlideToggle } from "@angular/material/slide-toggle";
+import { ProxyClient, SubProvider } from "../../../common/model/enums";
 import { IconButton } from "../../shared/icon-button/icon-button";
 
 @Component({
@@ -24,5 +25,20 @@ import { IconButton } from "../../shared/icon-button/icon-button";
     styleUrl: "./dashboard-sub.scss",
 })
 export class DashboardSub {
-    private subscriptionForm = new FormGroup({});
+    protected subscriptionForm = new FormGroup({
+        secret: new FormControl<string | null>(null, [ Validators.required ]),
+        url: new FormControl<string | null>(null, [ Validators.required ]),
+        client: new FormControl<string>(ProxyClient.Surge.toLowerCase(), { nonNullable: true }),
+        provider: new FormControl<string>(SubProvider.BosLife.toLowerCase(), { nonNullable: true }),
+        interval: new FormControl<number>(43200, {
+            nonNullable: true,
+            validators: [],
+        }),
+        strict: new FormControl<boolean>(true),
+    });
+
+    protected providers: SubProvider[] = Object.values(SubProvider);
+
+    protected clients: ProxyClient[] = Object.values(ProxyClient);
+
 }
