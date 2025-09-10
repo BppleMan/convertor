62812d2d88edcbbc4fccd29691ae44ec65902bc5
BppleMan <1286363484@qq.com>
2025-09-10 19:08:47 +0800

    <refactor> 移除echarts相关

--- dashboard/src/app/page/dashboard/dashboard.ts
+++ dashboard/src/app/page/dashboard/dashboard.ts
@@ -1,7 +1,4 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { BehaviorSubject, filter, interval } from "rxjs";
-import { DashboardService } from "../../service/dashboard.service";
-import { BenchAction } from "../shared/charts/bench-bar-chart/bench-bar-chart";
 import { DashboardInfo } from "./dashboard-info/dashboard-info";
 import { DashboardSub } from "./dashboard-sub/dashboard-sub";
 
@@ -16,23 +13,4 @@ import { DashboardSub } from "./dashboard-sub/dashboard-sub";
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Dashboard {
-    public readonly refresh = interval(1000);
-
-    public startBenchAction = new BehaviorSubject<BenchAction | null>(null);
-    public startBenchAction$ = this.startBenchAction.pipe(
-        filter(action => !!action),
-    );
-
-    public abortBenchAction = new BehaviorSubject<{} | null>(null);
-    public abortBenchAction$ = this.abortBenchAction.pipe(
-        filter(action => action !== null),
-    );
-
-    public startBench() {
-        this.startBenchAction.next({ url: DashboardService.HEALTH_ENDPOINT, count: 100 });
-    }
-
-    public abortBench() {
-        this.abortBenchAction.next({});
-    }
 }
